7e6e4e3bd3f89cc1a1d10defcfc64bbdf84a4471
James Mobus <mobusj@live.com>
2019-09-10 19:22:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+class Dog 
+  attr_accessor :name, :breed
+  
+  @@all = []
+  
+  def initialize(name)
+    self.name = name 
+    
+    save
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.print_all
+    @@all.each do |good_boy_or_girl|
+      puts good_boy_or_girl.name
+    end
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+end
