fffe9ad21e864ea53e740908dd37f448afa0d4a7
Christian Cain <christian22cain@yahoo.com>
2019-09-12 02:10:34 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+
+require 'pry'
+
+class Dog
+  attr_accessor :name
+  
+  @@all = []
+  @@names = []
+  
+  def initialize (name)
+    @name = name
+    save
+    
+    @@names << name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    all.clear
+  end
+  
+  def self.print_all
+    puts @@all.map{|element| element.name}
+  end
+  
+  
+  def save
+    @@all << self
+  end
+end
